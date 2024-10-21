607e8a806074b518fb4d2b70bdf82f1abfc17a5d
Olsney <104034829+Olsney@users.noreply.github.com>
2024-10-22 00:48:13 +0300

    SpawnController deleted

--- Assets/Scripts/BombSpawner.cs
+++ Assets/Scripts/BombSpawner.cs
@@ -1,7 +1,24 @@
-﻿namespace DefaultNamespace
+﻿using UnityEngine;
+
+namespace DefaultNamespace
 {
     public class BombSpawner : SpawnerBase<Bomb>
     {
         protected override string SpawnerName => "BombSpawner";
+        
+        [SerializeField] private CubeSpawner _cubeSpawner;
+        
+        private void OnEnable()
+        {
+            _cubeSpawner.CubeDestroyed += OnCubeDestroyed;
+        }
+
+        private void OnDisable()
+        {
+            _cubeSpawner.CubeDestroyed -= OnCubeDestroyed;
+        }
+
+        private void OnCubeDestroyed(Vector3 position) => 
+            Spawn(position);
     }
 }
